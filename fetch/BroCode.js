2e793b5700f9998e8182438b4c078307128a66f0
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-14 20:10:56 +0200

    we're going to use other method

--- fetch/BroCode.js
+++ fetch/BroCode.js
@@ -1,23 +1,32 @@
-const pokemonName = document.getElementById('pokemonName');
-const pokemonSprite = document.getElementById('pokemonSprite');
+// const pokemonName = document.getElementById('pokemonName');
+// const pokemonSprite = document.getElementById('pokemonSprite');
 
+
+// fetchImage()
+// function fetchImage() {
+//     const pokemonImg = pokemonName.value.toLowerCase();
+
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonImg}`)
+//         .then(res => {
+//             if (!res.ok) {
+//                 throw Error('There\'s no image because of link');
+//                 // console.log('There\'s no image because of link');
+//             }
+//             return res.json();
+//         })
+//         .then(data => {
+//             let imgData = data.sprites.front_default
+//             pokemonSprite.src = imgData;
+//             pokemonSprite.style.display = 'block'
+//         })
+//         .catch(error => console.log(error));
+// }
 
 fetchImage()
 function fetchImage() {
-    const pokemonImg = pokemonName.value.toLowerCase();
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonImg}`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error('There\'s no image because of link');
-                // console.log('There\'s no image because of link');
-            }
-            return res.json();
-        })
-        .then(data => {
-            let imgData = data.sprites.front_default
-            pokemonSprite.src = imgData;
-            pokemonSprite.style.display = 'block'
-        })
-        .catch(error => console.log(error));
+    const pokemonName = document.getElementById('pokemonName');
+    const pokemonSprite = document.getElementById('pokemonSprite');
+
+
 }
