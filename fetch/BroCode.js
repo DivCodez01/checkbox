6e4b93f58a39f053c1d2bf4aded7c47cc25c3dd7
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-14 19:55:05 +0200

    fetch iamge on click event

--- fetch/BroCode.js
+++ fetch/BroCode.js
@@ -1,4 +1,13 @@
 
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(res => res.json())
-    .then(data => console.log(data))
+fetchImage()
+function fetchImage() {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(res => {
+            if (!res.ok) {
+                console.log('There\'s no image because of link')
+            }
+            return res.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
